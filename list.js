14f01260fd128d117d6ac0ec5116cbce8e67f54f
amalkdl007 <93526375+amalkdl007@users.noreply.github.com>
2021-11-17 20:44:40 +0530

    New git
    grocery challenge

---
+++ list.js
@@ -0,0 +1,18 @@
+
+        var table = document.getElementsByClassName("table");
+
+        table[0].setAttribute("hidden",true);
+
+        function reset(){
+
+        var table = document.getElementsByClassName("table");
+
+        table[0].removeAttribute("hidden","true");
+
+        }
+
+     
+
+
+
+
